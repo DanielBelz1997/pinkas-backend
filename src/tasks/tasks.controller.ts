6cd8a3ce6a5605a7963a403fe0cab38c4ad430a8
BelzDaniel <BelzDaniel6@gmail.com>
2024-08-02 13:51:43 +0300

    feat: creating the basic routes for tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  @Get() // GET tasks
+  findAll() {
+    return [];
+  }
+
+  @Get(':id') // GET /tasks/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /tasks
+  create(@Body() task: { id: string; name: string; checked: boolean }) {
+    return task;
+  }
+
+  @Patch(':id') // PATCH /tasks/:id
+  update(@Param('id') id: string, @Body() taskUpdate: {}) {
+    return { id, ...taskUpdate };
+  }
+
+  @Delete(':id') // DELETE /tasks/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 @Module({
-  controllers: [],
-  providers: [],
+  controllers: [TasksController],
+  providers: [TasksService],
 })
 export class TasksModule {}
